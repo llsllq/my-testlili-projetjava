f964de7aa837d33362e733f675529fe5e54ec681
adllq <adllq@serveur-dev.tec.saint-louis.local>
2014-03-25 18:00:36 +0100

    Mise à jour repositiry local avec nouveau ProPrin

--- TestJavaGit/src/ProgPrin.java
+++ TestJavaGit/src/ProgPrin.java
@@ -6,8 +6,7 @@ public class ProgPrin {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("test version 2");
+System.out.println("test mise à jour");
 
 	}
-
 }
